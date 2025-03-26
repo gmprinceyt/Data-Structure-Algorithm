06c4a45bae0ef8470763981968b369692cc7e3ae
PRINCE KUAMR <gmprince420@gmail.com>
2025-03-26 18:22:45 +0530

    bug fix in algo

--- Array/Day16.cpp
+++ Array/Day16.cpp
@@ -8,10 +8,10 @@ void lacturePractic()
     //? Array Selection Sorting 
     int arr[6] = {10, 8, 2, 3, 1, 4};
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 6-1; i++)
     {
         int in = i;
-        for (int j = i + 1; j < 6-1; j++)
+        for (int j = i + 1; j < 6; j++)
         {
             if (arr[j] < arr[in])
                 in = j;
