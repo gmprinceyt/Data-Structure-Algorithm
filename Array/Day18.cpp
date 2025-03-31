463e7a06de5f61409bce8758cf76f9598526908c
PRINCE KUAMR <gmprince420@gmail.com>
2025-03-31 16:11:21 +0530

    3: Selection Sort Algorithm to sort the array of char in ascending order.

--- Array/Day18.cpp
+++ Array/Day18.cpp
@@ -34,14 +34,14 @@ void lacturePractic()
 void HomeWorkQuestion()
 {
 
-    int arr[1000];
-    int n;
-    cout << "Enter Size Of Array: ";
-    cin >> n;
-    cout << "Enter Array Of Value: ";
+    // int arr[1000];
+    // int n;
+    // cout << "Enter Size Of Array: ";
+    // cin >> n;
+    // cout << "Enter Array Of Value: ";
 
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
+    // for (int i = 0; i < n; i++)
+    //     cin >> arr[i];
 
     //? 1: Selection Sort Algorithm to sort the array of integers in decreasing order.
     // Algorithm
@@ -57,25 +57,51 @@ void HomeWorkQuestion()
     //     }
     // }
 
-    //? 2: Selection Sort Algorithm to sort the array of integers in increasing order by taking the highest number to last place. Question was explained in the class. 
+    //? 2: Selection Sort Algorithm to sort the array of integers in increasing order by taking the highest number to last place. Question was explained in the class.
 
     // algorithm
 
-    for (int i = n-2; i >= 0; i--)
+    // for (int i = n-2; i >= 0; i--)
+    // {
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         if (arr[j] > arr[j+1])
+    //             swap(arr[j], arr[j+1]);
+    //         else
+    //             break;
+    //     }
+    // }
+
+    // Array Printing
+
+    // for (int i = 0; i < n; i++)
+    //     cout << arr[i] << " ";
+
+    //? 3: Selection Sort Algorithm to sort the array of char in ascending order.
+
+    char arr[100];
+    int n;
+    cout << "Enter Size Of Array: ";
+    cin >> n;
+    cout << "Enter Value Of Array: ";
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // algorithm
+    for (int i = 1; i < n; i++)
     {
-        for (int j = i; j < n; j++)
+        for (int j = i; j > 0; j--)
         {
-            if (arr[j] > arr[j+1])
-                swap(arr[j], arr[j+1]);
-            else 
+            if (arr[j] < arr[j - 1])
+                swap(arr[j], arr[j - 1]);
+            else
                 break;
         }
     }
 
-    // Array Printing
-
     for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    cout << arr[i] << " ";
 }
 
 int main()
