7357dec856ef6823a886601d316ee799749de03c
PRINCE KUAMR <gmprince420@gmail.com>
2025-07-22 14:53:09 +0530

    more Pattern

--- Pattern/assignment/Class2.cpp
+++ Pattern/assignment/Class2.cpp
@@ -68,10 +68,7 @@ int main (){
     //     for (int j = 0; j < n-1-i; j++){
     //         cout<<"  ";
     //     }
-    //     for (int j = 0; j <= i; j++){
-    //         cout<<"* ";
-    //     }
-    //     for (int j = 1; j <= i; j++){
+    //     for (int j = 0; j <= i+i; j++){
     //         cout<<"* ";
     //     }
     //     cout << endl;
@@ -83,14 +80,113 @@ int main (){
     //     for (int j = 0; j < n-i; j++){
     //         cout<<"  ";
     //     }
-    //     for (int j = 0; j <  i; j++){
-    //         cout<<"* ";
-    //     }
-    //     for (int j = 1; j <  i; j++){
+    //     for (int j = 0; j <= i+i-2 ; j++){
     //         cout<<"* ";
     //     }
     //     cout << endl;
     // };
+
+    //? 8. Hollow inverted Heft Pyramid
+
+    /*
+    col-1
+    * * * * * - row 
+    *     *
+    *   * 
+    * *
+    * 
+    */
+
+    // for (int i = n; i > 0; i--){
+    //     for (int j = 0; j < i; j++){
+    //         if (i == n || j==0 || j==i-1 ){
+    //             cout << " * ";
+    //         }else{
+    //             cout <<"   ";
+    //         }
+    //     }
+    //     cout << endl;
+    // };
+
+    //? 9. full hollow triangle Pyramid
+
+    /*
+                    *
+                   * *
+                  *   * 
+                 *     * 
+                *       *
+               * * * * * *
+    */
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n-i-1; j++){
+    //         cout << "   ";
+    //     }
+    //     for (int j = 0; j <= i+i; j++){
+    //         if (j == 0 || i == n-1 || j == i+i){
+    //             cout << " * ";
+    //         }else {
+    //             cout << "   ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    //? 10. Solid Diamond
+
+    // for (int i = 0; i < n ; i++){
+    //     for (int j = 0; j < n-i-1; j++){
+    //         cout << "   ";
+    //     }
+    //     for (int j = 0; j <= i+i; j++){
+    //         cout << " * ";
+    //     };
+    //     cout << endl;
+    // }
+    // for (int i = n; i > 0 ; i--){
+    //     for (int j = 0; j < n-i; j++){
+    //         cout << "   ";
+    //     }
+    //     for (int j = 0; j <= i+i-2; j++){
+    //         cout << " * ";
+    //     };
+    //     cout << endl;
+    // }
+
+
+    //? 11. Hollow Solid Diamond
+
+    // for (int i = 0; i < n ; i++){
+    //     for (int j = 0; j < n-i-1; j++){
+    //         cout << "   ";
+    //     }
+    //     for (int j = 0; j <= i+i; j++){
+    //         if (j == 0 || j == i+i){
+    //             cout << " * ";
+    //         }else {
+    //             cout << "   ";
+    //         }
+    //     };
+    //     cout << endl;
+    // }
+    // for (int i = n; i > 0 ; i--){
+    //     for (int j = 0; j < n-i; j++){
+    //         cout << "   ";
+    //     }
+    //     for (int j = 0; j <= i+i-2; j++){
+    //        if (j == 0 || j == i+i-2){
+    //             cout << " * ";
+    //         }else {
+    //             cout << "   ";
+    //         }
+    //     };
+    //     cout << endl;
+    // }
+
+
+
    
    return 0;
 }
